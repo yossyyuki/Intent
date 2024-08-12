52d74187ae3d463e8609339c8914bf46af8bf8ea
yossyyuki <studyingapp0716@gmail.com>
2024-08-12 16:16:12 +0900

    mainactivityからfragmentへの遷移を追加（成功？）

--- app/src/main/java/jp/techacademy/yoshiyuki/okumura/intent/TopFragment.kt
+++ app/src/main/java/jp/techacademy/yoshiyuki/okumura/intent/TopFragment.kt
@@ -3,14 +3,44 @@ package jp.techacademy.yoshiyuki.okumura.intent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 //import jp.techacademy.yoshiyuki.okumura.intent.databinding.ActivityMainBinding
 import jp.techacademy.yoshiyuki.okumura.intent.databinding.FragmentTopBinding
 
-class TopFragment : AppCompatActivity()
+open class TopFragment : Fragment() {
 
-private lateinit var binding: FragmentTopBinding
+    private var _binding: FragmentTopBinding? = null
 
+    private val binding: FragmentTopBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTopBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button1.setOnClickListener {
+            binding.button1.text = "？？"
+
+            fun onDestroyView() {
+                super.onDestroyView()
+                _binding = null
+
+            }
+        }
+    }
+    companion object {
+        //
+        private lateinit var binding: FragmentTopBinding
+    }
+}
 
 
 

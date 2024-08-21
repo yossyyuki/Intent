3515263e0bca3b0b894f4ea4a42f508d6b2009c5
yossyyuki <studyingapp0716@gmail.com>
2024-08-22 05:42:44 +0900

    ストップウォッチのコードをMeasurementFragment追加

--- app/src/main/java/jp/techacademy/yoshiyuki/okumura/intent/MeasurementFragment.kt
+++ app/src/main/java/jp/techacademy/yoshiyuki/okumura/intent/MeasurementFragment.kt
@@ -1,8 +1,9 @@
 package jp.techacademy.yoshiyuki.okumura.intent
 
-import android.content.Intent
+//import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,11 +28,77 @@ open class MeasurementFragment : Fragment() {
         binding.button.setOnClickListener {
 
         }
-    }
+        binding.button.setOnClickListener {
+            // FragmentManagerの取得
+            // トランザクションの生成・コミット
+            val ft = parentFragmentManager.beginTransaction()
+            ft.replace(R.id.container, MeasurementFragment())
+            ft.commit()
+            ft.addToBackStack(null)
+        }
+        //ストップウォッチ用のコード
+        class MeasurementFragment : Fragment() {
+            val handler = Handler()                      //
+            var timeValue = 0                              // 秒カウンター
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+            //
+            override fun onCreated(savedInstanceState: Bundle?) {
+                super.onViewCreated(view, savedInstanceState)
+                binding.start.setOnClickListener {
+
+//                // Handler(スレット間通信：イベントキュー？)
+                    val runnable = object : Runnable {
+                        //                    // メッセージ受信が有った時かな?
+                        override fun run() {
+                            timeValue++                      // 秒カウンタ+1
+                            timeToText(timeValue)?.let {        // timeToText()で表示データを作り
+                                binding.start.text = it           // timeText.textへ代入(表示)
+                            }
+                            handler.postDelayed(this, 1000)  // 1000ｍｓ後に自分にpost
+                        }
+                    }
+//
+//                // startボタン押された時(setOnClickListener)の処理
+                    binding.start.setOnClickListener {
+                        handler.post(runnable)                // 最初のキュー登録
+//                }
+//                // stopボタン押された時の処理
+////                stop.setOnClickListener {
+////                    handler.removeCallbacks(runnable)      // キューキャンセル
+////                }
+//                // resetボタン押された時の処理
+////                reset.setOnClickListener {
+////                    handler.removeCallbacks(runnable)      // キューキャンセル
+////                    timeValue = 0                          // 秒カウンタークリア
+////                    timeToText()?.let {                  // timeToText()で表示データを作り
+////                        timeText.text = it                // timeText.textに表示
+//                    }
+//                }
+//            }
+//
+//            // 表示
+//            private fun timeToText(time: Int = 0): String? {
+//                return if (time < 0) {
+//                    null                                    // 時刻が0未満の場合 null
+//                } else if (time == 0) {
+//                    "00:00:00"                            // ０なら
+//                } else {
+//                    val h = time / 3600
+//                    val m = time % 3600 / 60
+//                    val s = time % 60
+//                    "%1$02d:%2$02d:%3$02d".format(h, m, s)  // 表示に整形
+//                }
+//            }
+
+//        override fun onDestroyView() {
+//            super.onDestroyView()
+//            _binding = null
+                    }
+                }
+            }
+        }
     }
 }
+
+
 
